fbc431cb3c99358f405176e1e9dcf162ee2fe9ec
chowder <16789070+chowder@users.noreply.github.com>
2023-07-26 12:31:06 +0100

    Add workflow to build wheel

---
+++ .github/workflows/scripts/build_setup.sh
@@ -0,0 +1,13 @@
+set -e
+
+TA_LIB_TGZ="ta-lib-0.4.0-src.tar.gz"
+TA_LIB_URL="http://prdownloads.sourceforge.net/ta-lib/$TA_LIB_TGZ"
+
+wget -O "/tmp/$TA_LIB_TGZ" $TA_LIB_URL
+pushd /tmp
+tar -zxvf $TA_LIB_TGZ
+popd
+pushd /tmp/ta-lib
+./configure
+make install
+popd
